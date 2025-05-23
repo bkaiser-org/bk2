ba875400c724796643b58efc712ee0ffab9d89aa
Bruno Kaiser <bruno@bkaiser.ch>
2025-05-23 13:42:03 +0200

    initial major redesign of bk2

---
+++ apps/test-app/src/app/app.config.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { PreloadAllModules, RouteReuseStrategy, provideRouter, withComponentInputBinding, withEnabledBlockingInitialNavigation, withPreloading } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+// plain firebase with rxfire
+import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from 'firebase/app-check';
+
+import { ENV } from '@bk2/shared/config';
+import { environment } from '../environments/environment';
+
+// i18n with transloco
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslateModule } from '@ngx-translate/core';
+import { I18nService, TranslocoHttpLoader } from '@bk2/shared/i18n';
+
+const app = initializeApp(environment.firebase);
+
+if (environment.production === false) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (<any>window).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+// Create a ReCaptchaEnterpriseProvider instance using the reCAPTCHA Enterprise
+// site key and pass it to initializeAppCheck().
+initializeAppCheck(app, {
+  provider: new ReCaptchaEnterpriseProvider(
+    environment.firebase.appcheckRecaptchaEnterpriseKey
+  ),
+  isTokenAutoRefreshEnabled: true, // Set to true to allow auto-refresh.
+});
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    { provide: ENV, useValue: environment },
+    provideAnimations(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({ useSetInputAPI: true }),
+    provideRouter(
+      appRoutes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules),
+      withEnabledBlockingInitialNavigation()
+    ),
+
+    importProvidersFrom(TranslateModule.forRoot()),
+
+    // Browser and HttpClient
+    importProvidersFrom(BrowserModule),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'de', 'fr', 'es', 'it'],
+        defaultLang: 'de',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+    I18nService
+  ],
+};
