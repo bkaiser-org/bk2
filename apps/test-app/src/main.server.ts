00dcdd269d8ac89594c274b09a0c190f3b5a7e64
Bruno Kaiser <bruno@bkaiser.ch>
2025-07-12 14:03:41 +0200

    moving import of dom.mock into main.server

--- apps/test-app/src/main.server.ts
+++ apps/test-app/src/main.server.ts
@@ -1,3 +1,5 @@
+import '../../dom.mock.js'; // IMPORTANT: Must be the first import
+
 /***************************************************************************************************
  * Zone.js wird von Angular für die Change Detection benötigt (für SSR).
  * Es muss importiert werden, NACHDEM der DOM-Mock vorhanden ist.
