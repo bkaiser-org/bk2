e09f4499f0e4f62d13a8f728d32285a0b2379ea8
Bruno Kaiser <bruno@bkaiser.ch>
2025-07-13 10:15:54 +0200

    fix: adapting mock-logic to new ssr syntax.

--- apps/test-app/src/main.server.ts
+++ apps/test-app/src/main.server.ts
@@ -1,3 +1,19 @@
+/***************************************************************************************************
+ * BROWSER-POLYFILLS FÜR DIE NODE.JS-UMGEBUNG
+ *
+ * Diese müssen als ALLERERSTES importiert werden, damit globale Objekte wie `window`,
+ * `document` und `customElements` für den SSR/Prerender-Prozess verfügbar sind.
+ ***************************************************************************************************/
+import './ssr.mocks';
+
+/***************************************************************************************************
+ * Zone.js für den Server
+ *
+ * Muss NACH den DOM-Polyfills importiert werden.
+ ***************************************************************************************************/
+import 'zone.js/node';
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
