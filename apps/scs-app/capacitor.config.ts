d291460742536913a24fd5824e2b83a0d11081e4
Bruno Kaiser <bruno@bkaiser.ch>
2026-03-10 20:57:26 +0100

    add ios app (trying to achieve a better video call notification)

---
+++ apps/scs-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.bkaiser.scs',
+  appName: 'Seeclub Stäfa',
+  webDir: '../../dist/apps/scs-app/browser'
+};
+
+export default config;
